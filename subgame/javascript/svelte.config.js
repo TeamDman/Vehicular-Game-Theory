f2aec67487a4101ee8faa148d473e2be7223b15c
TeamDman <TeamDman9201@gmail.com>
2022-06-30 15:12:05 -0400

    render static, add basepath

--- subgame/javascript/svelte.config.js
+++ subgame/javascript/svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,9 +7,17 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
+	
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		trailingSlash: "always",
+		prerender: {
+			default: true,
+		},
+		paths: {
+			base: "/vehicle-risk"
+		},
 	}
 };
 
